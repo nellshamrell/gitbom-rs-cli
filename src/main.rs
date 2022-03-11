d224e57f914a828c77a4157f02a46f9f077360c0
Nell Shamrell <nells@microsoft.com>
2022-03-10 16:28:04 -0800

    messy implementation of call to gitbom-rs library, but it works!
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use gitbom::{HashAlgorithm, GitOid};
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+#[derive(Parser)]
+struct Cli {
+    path: String
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    println!("args: {:?}", args.path);
+
+    let file = File::open(args.path)?;
+    println!("file: {:?}", file);
+
+    let reader = BufReader::new(file);
+
+    let new_gitoid = GitOid {
+        hash_algorithm: HashAlgorithm::SHA1
+    };
+
+    let result = new_gitoid.generate_git_oid_from_buffer(reader, 11);
+    println!("result: {:?}", result);
+
+    Ok(())
 }
