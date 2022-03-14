99b0a3111e6024b614d9e8ec59e221501f7d65b8
Nell Shamrell <nellshamrell@gmail.com>
2022-03-14 16:16:45 -0700

    adds more command support
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,59 @@
 use gitbom::{HashAlgorithm, GitOid};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use std::fs::File;
 use std::io::{BufReader};
 
+/// A GitBom CLI written in Rust
 #[derive(Parser)]
+#[clap(name = "gitbom")]
+#[clap(about = "A CLI for creating GitBom documents", long_about = None)]
 struct Cli {
-    path: String
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Creates a GitBOM for a single file
+    #[clap(arg_required_else_help = true)]
+    Bom {
+        /// File to generate a GitBOM for
+        file: String
+    },
+
+    /// Creates a GitBOM for a directory
+    #[clap(arg_required_else_help = true)]
+    ArtifactTree {
+        /// Directory to generate a GitBOM for
+        directory: String
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
-    let file = File::open(args.path)?;
-    let file_length = file.metadata()?.len();
+    match &args.command {
+        Commands::Bom { file } => {
+            println!("Generating GitBOM for {}", file);
+            let file = File::open(file)?;
+            let file_length = file.metadata()?.len();
+            let reader = BufReader::new(file);
+            let new_gitoid = GitOid::new(HashAlgorithm::SHA1);
 
-    let reader = BufReader::new(file);
-    let new_gitoid = GitOid::new(HashAlgorithm::SHA1);
 
+            let result = new_gitoid.generate_git_oid_from_buffer(reader, file_length as usize);
 
-    let result = new_gitoid.generate_git_oid_from_buffer(reader, file_length as usize);
-    println!("result: {:?}", result.unwrap());
+            match result {
+                Ok(r) => println!("{:?}", r),
+                Err(e) => println!("Error generating the GitBOM: {:?}", e),
+            }
 
-    Ok(())
+            Ok(())
+        },
+        Commands::ArtifactTree { directory } => {
+            println!("Generating GitBOM for {}", directory);
+            println!("Not implemented yet. Patience.");
+            Ok(())
+        }
+    }
 }
