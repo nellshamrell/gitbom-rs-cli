0eec1ab9afb429fb40b5591f43715e1642dc6838
Nell Shamrell <nellshamrell@gmail.com>
2022-03-14 15:30:38 -0700

    seems to be working
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use gitbom::{HashAlgorithm, GitOid};
 use clap::Parser;
 use std::fs::File;
+use std::hash::Hash;
 use std::io::{BufReader, Read};
 
 #[derive(Parser)]
@@ -13,16 +14,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("args: {:?}", args.path);
 
     let file = File::open(args.path)?;
-    println!("file: {:?}", file);
+    let file_length = file.metadata()?.len();
 
     let reader = BufReader::new(file);
+    
+    let new_gitoid = GitOid::new(HashAlgorithm::SHA1);
 
-    let new_gitoid = GitOid {
-        hash_algorithm: HashAlgorithm::SHA1
-    };
 
-    let result = new_gitoid.generate_git_oid_from_buffer(reader, 11);
-    println!("result: {:?}", result);
+    let result = new_gitoid.generate_git_oid_from_buffer(reader, file_length as usize);
+    println!("result: {:?}", result.unwrap());
 
     Ok(())
 }
