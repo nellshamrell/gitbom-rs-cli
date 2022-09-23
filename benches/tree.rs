00b410a561912468b0147b2a62903086634b0994
Nell Shamrell <nellshamrell@gmail.com>
2022-09-23 14:59:25 -0700

    makes the benchmark test properly set up and clean up after itself
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- benches/tree.rs
+++ benches/tree.rs
@@ -3,16 +3,22 @@
 use assert_cmd::Command;
 extern crate test;
 use test::Bencher;
+use std::fs;
 
 #[bench]
 fn bench_artifact_tree_output_test(b: &mut Bencher) -> Result<(), Box<dyn std::error::Error>> {
+    fs::create_dir_all("temp_test_dir_1")?;
+
     let mut cmd = Command::cargo_bin("gitbom-cli")?;
-    cmd.arg("artifact-tree").arg("tests/fixtures/large_directory");
+    cmd.arg("artifact-tree").arg("../tests/fixtures/large_directory");
+    cmd.current_dir("temp_test_dir_1");
 
     b.iter(||
         // Executes the command
         cmd.unwrap()
     );
+
+    fs::remove_dir_all("temp_test_dir_1")?;
 
     Ok(())
 }
